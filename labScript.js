f7084af9db442d824129954c12cba4a343e6088f
daotiensyyyy <55313833+daotiensyyyy@users.noreply.github.com>
2019-11-02 14:23:27 +0700

    update

--- labScript.js
+++ labScript.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
+	// $('#part-1').on('click', function(event) {
+	// 	event.preventDefault();
 
-$('.booking a').click(function(event) {
-event.preventDefault();
+	// 	$('html, body').animate({scrollTop: 1500},400);
+	// });
 
-part = $(this).attr('href'); // lấy id của thẻ h2 tương ứng
+	$('.booking-btn a').click(function(event) {
+		event.preventDefault();
 
-position = $(part).offset().top; // tìm vị trí thẻ h2
+		part = $(this).attr('href'); // lấy id của thẻ h2 tương ứng
+		// console.log(part);
 
-$('html, body').animate({scrollTop: position},1400,'easeInOutSine');
-});
+		position = $(part).offset().top; // tìm vị trí thẻ h2
+
+		// $('#part-1').offset().top;
+		// hiệu ứng
+		$('html, body').animate({scrollTop: position},1400,'easeInOutSine');
+	});
 });
